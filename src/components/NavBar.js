48b2b9ba5bd90ea55132427537d602ba87d908d4
Oscar Leung <oscarleung1@gmail.com>
2021-11-03 12:45:58 -0700

    Added drop down menu for opening and closing for Contact Button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Menu } from "@mui/material";
+import { Menu, MenuItem } from "@mui/material";
 import { Button } from "@mui/material";
 
 export default function NavBar() {
@@ -15,11 +15,32 @@ export default function NavBar() {
   return (
     <div>
       <Button variant="outlined">Home</Button>
-      <Button variant="outlined">About Me</Button>
       <Button variant="outlined">Portfolio</Button>
       <Button variant="outlined">Resume</Button>
       <Button variant="outlined">Blog</Button>
-      <Button variant="outlined">Contact</Button>
+      <Button
+        id="contact-button"
+        aria-controls="contact-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        variant="outlined"
+      >
+        Contact
+      </Button>
+
+      <Menu
+        id="contact-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{ "aria-labelledby": "contact-button" }}
+      >
+        <MenuItem>
+          <Button onClick={handleClose}>About Me</Button>
+          Email Address: oscarleung1@gmail.com
+        </MenuItem>
+      </Menu>
     </div>
   );
 }
