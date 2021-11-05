4b52e53cae79ef1471c6d208ef8a0817c1f837a8
Oscar Leung <oscarleung1@gmail.com>
2021-11-04 17:45:48 -0700

    Added Contact Page

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+
+const ContactPage = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <label htmlFor="name">
+          Name
+          <input
+            id="name"
+            value={name}
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            id="email"
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="phone">
+          Phone
+          <input
+            id="phone"
+            value={phone}
+            placeholder="(###)###-####"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="address">
+          Address
+          <input
+            id="address"
+            value={address}
+            placeholder="address"
+            onChange={(e) => setAddress(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="subject">
+          Subject
+          <input
+            id="Subject"
+            value={subject}
+            placeholder="subject"
+            onChange={(e) => setSubject(e.target.value)}
+          ></input>
+        </label>
+        <label htmlFor="message">
+          Message
+          <input
+            id="Message"
+            value={message}
+            placeholder="message"
+            onChange={(e) => setMessage(e.target.value)}
+          ></input>
+        </label>
+        <Button color="secondary" variant="contained">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
