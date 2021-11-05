61d20bd84242b3d19c4a6d201036f05d5e1a7170
Oscar Leung <oscarleung1@gmail.com>
2021-11-05 15:53:04 -0700

    Added Themeprovider on app component

--- src/App.js
+++ src/App.js
@@ -3,47 +3,42 @@ import "./App.css";
 import { Button, Link } from "@mui/material";
 import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Routes,
-} from "react-router-dom";
-import BlogPage from "./pages/BlogPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import theme from "./components/theme";
+import { ThemeProvider } from "@mui/system";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button variant="contained"> Hello world!</Button>
-        <NavBar></NavBar>
-        {/* <HomePage></HomePage> */}
-        {/* <BlogPage /> */}
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <NavBar></NavBar>
 
-        <Router>
-          <div>
-            <Link to="/home">Homepage</Link>
-            <Routes>
-              <Route path="/home">
-                <HomePage />
-              </Route>
-            </Routes>
-          </div>
-        </Router>
-      </header>
-    </div>
+          <Router>
+            <div>
+              <Link to="/home">Homepage</Link>
+              <Routes>
+                <Route path="/home">
+                  <HomePage />
+                </Route>
+              </Routes>
+            </div>
+          </Router>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+import { lightBlue, lightGreen } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: lightBlue,
+    secondary: lightGreen,
+  },
+});
+
+export default theme;
