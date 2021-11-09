18e178e29608e0b5fb10f5b4002fa9c4764feb5f
Oscar Leung <oscarleung1@gmail.com>
2021-11-08 17:14:44 -0800

    Added back theme

--- src/App.js
+++ src/App.js
@@ -7,41 +7,45 @@ import BlogPage from "./pages/BlogPage";
 import "./App.css";
 import { Button } from "@mui/material";
 import ErrorBoundary from "./ErrorBoundary";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./components/theme";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ErrorBoundary>
-          <Router>
-            <nav>
-              <Button variant="outlined">
-                <Link to="/">Home</Link>
-              </Button>
-              <Button variant="outlined">
-                <Link to="portfolio">Portfolio</Link>
-              </Button>
-              <Button variant="outlined">
-                <Link to="resume">Resume</Link>
-              </Button>
-              <Button variant="outlined">
-                <Link to="blog">Blog</Link>
-              </Button>
-              <Button variant="outlined">
-                <Link to="contact-me">Contact Me</Link>
-              </Button>
-            </nav>
+        <ThemeProvider theme={theme}>
+          <ErrorBoundary>
+            <Router>
+              <nav>
+                <Button variant="outlined">
+                  <Link to="/">Home</Link>
+                </Button>
+                <Button variant="outlined">
+                  <Link to="portfolio">Portfolio</Link>
+                </Button>
+                <Button variant="outlined">
+                  <Link to="resume">Resume</Link>
+                </Button>
+                <Button variant="outlined">
+                  <Link to="blog">Blog</Link>
+                </Button>
+                <Button variant="outlined">
+                  <Link to="contact-me">Contact Me</Link>
+                </Button>
+              </nav>
 
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/portfolio" element={<PortfolioPage />} />
-              <Route path="/resume" element={<ResumePage />} />
-              <Route path="/blog" element={<BlogPage />} />
-              <Route path="/contact-me" element={<ResumePage />} />
-              <Route path="*" element={<ErrorBoundary />} />
-            </Routes>
-          </Router>
-        </ErrorBoundary>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/portfolio" element={<PortfolioPage />} />
+                <Route path="/resume" element={<ResumePage />} />
+                <Route path="/blog" element={<BlogPage />} />
+                <Route path="/contact-me" element={<ResumePage />} />
+                <Route path="*" element={<ErrorBoundary />} />
+              </Routes>
+            </Router>
+          </ErrorBoundary>
+        </ThemeProvider>
       </header>
     </div>
   );
